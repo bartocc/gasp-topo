e4b905f95617f193e9d4041ccef8d60e0df5ee52
Julien Palmas <julien@cruxandco.com>
2009-02-05 13:41:55 +0100

    cleaned a bit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,46 +1,36 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
-var gdir = null;
-
 document.observe("dom:loaded", function() {
-  // set the map
-  var map = new GMap2($('map'));
-  map.setCenter(new GLatLng(44.013655, 1.278026), 13);
-  
-  topoGetAltitude(46, 1, foo, 'bar');
-  
+  controller.init();
 });
-
-function foo(altitude, context) {
-  alert(altitude); // => undefined
-  alert(context); // => undefined
-}
 
 var controller = {
   
+  init: function() {
+    var toulouse = new GLatLng(43.604184,1.442943);
+    this.map = new GMap2($('map'));
+    this.map.setCenter(toulouse, 13);
+  },
+  
   topocode: function() {
-    gdir= new GDirections(map);
-    GEvent.addListener(gdir, "load", controller.loadCallback.bind(controller));
-    $('values').innerHTML = '';
-    gdir.load("from: " + $F('from') + " to: " + $F('to'), {'getSteps': true});
+    this.gdir = new GDirections(this.map);
+    GEvent.addListener(this.gdir, "load", controller.loadCallback.bind(controller));
+    var request = "from: " + $F('from') + " to: " + $F('to');
+    this.gdir.load(request, {'getSteps': true});
   },
   
   loadCallback: function() {
-    var route = gdir.getRoute(0);
+    var route = this.gdir.getRoute(0);
     var num_steps = route.getNumSteps();
     
     for (i=0 ; i < num_steps ; i++) {
       var step = route.getStep(i);
       var coords = step.getLatLng();
-      topoGetAltitude(coords.lat(), coords.lng(), this.topocodeCallback.bind(this), step);
+      topoGetAltitude(coords.lat(), coords.lng(), this.topocodeCallback.bind(this));
     }
     
   },
   
   topocodeCallback: function(altitude, context) {
-    var step = context;
-    $('values').innerHTML += (step.getDistance().meters + 'm ; ' + altitude + 'm<br />');
+    $('values').innerHTML += (altitude + 'm<br />');
   }
   
 }
